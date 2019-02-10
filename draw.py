88d60d9828f9f18a67d653f83992e9d906548681
Kevinlyy <batmanthelegendly@gmail.com>
2019-02-10 12:08:56 -0500

    finished

--- draw.py
+++ draw.py
@@ -1,4 +1,58 @@
 from display import *
 
 def draw_line( x0, y0, x1, y1, screen, color ):
-    pass
+
+    if x0 > x1:
+        draw_line(x1,y1,x0,y0,screen, color)
+        return
+
+    x = x0
+    y = y0
+    A = y1 - y0
+    B = -1 *(x1 - x0)
+
+    # Octant 1
+    if 0 <= A and A <= -B:
+        d = B + 2 * A
+        while x <= x1:
+            plot(screen,color,x,y)
+            if d >= 0:
+                y += 1
+                d += B * 2
+            x += 1
+            d += A * 2
+
+    # Octant 2
+    elif 0 <= A and -B <= A:
+        d = A + 2 * B
+        while y <= y1:
+            plot(screen,color,x,y)
+            if d <= 0:
+                x += 1
+                d += A * 2
+            y += 1
+            d += B * 2
+
+    # Octant 7
+    elif A <= 0 and A <= B:
+        d = A + -2 * B
+        while y1 <= y:
+            plot(screen,color,x,y)
+            if d >= 0:
+                x += 1
+                d += A * 2
+            y += -1
+            d += -2 * B
+
+    # Octant 8
+    elif A <= 0 and B <= A:
+        d = 2 * A + -B
+        while x1 >= x:
+            plot(screen,color,x,y)
+            if d <= 0:
+                y += -1
+                d += -2 * B
+            x += 1
+            d += A * 2
+    else:
+        return
--- main.py
+++ main.py
@@ -2,7 +2,57 @@ from display import *
 from draw import *
 
 screen = new_screen()
-color = [ 0, 255, 0 ]
+color = [ 255, 255, 255 ]
+
+draw_line( 250, 250, 500, 500, screen, color )
+draw_line( 250, 250, 500, 350, screen, color )
+draw_line( 250, 250, 500,  50, screen, color )
+draw_line( 250, 250, 500, 150, screen, color )
+draw_line( 250, 250, 350,   0, screen, color )
+draw_line( 250, 250, 250,   0, screen, color )
+draw_line( 250, 250, 350, 500, screen, color )
+draw_line( 250, 250, 250, 500, screen, color )
+draw_line( 250, 250, 500, 250, screen, color )
+
+draw_line( 250, 250,   0,   0, screen, color )
+draw_line( 250, 250,   0, 150, screen, color )
+draw_line( 250, 250, 150,   0, screen, color )
+draw_line( 250, 250,   0, 500, screen, color )
+draw_line( 250, 250,   0, 350, screen, color )
+draw_line( 250, 250, 150, 500, screen, color )
+draw_line( 250, 250, 250, 500, screen, color )
+draw_line( 250, 250,   0, 250, screen, color )
+draw_line( 250, 250, 250,   0, screen, color )
+
+#-------------------------------------------------------
+#top lines
+# draw_line(50,400,250,430,screen,[0,150,255])
+# draw_line(255,432,450,460,screen,[0,150,255])
+#
+# #bottom lines
+# draw_line(50,100,250,70,screen,[0,150,255])
+# draw_line(255,68,450,38,screen,[0,150,255])
+#
+# #vertical lines
+# #leftmost
+# draw_line(50,400,50,255,screen,[0,150,255])
+# draw_line(50,100,50,245,screen,[0,150,255])
+# #left-middle
+# draw_line(250,430,250,255,screen,[0,150,255])
+# draw_line(250,245,250,70,screen,[0,150,255])
+# #right-middle
+# draw_line(255,432,255,255,screen,[0,150,255])
+# draw_line(255,245,255,68,screen,[0,150,255])
+# #rightmost
+# draw_line(450,460,450,255,screen,[0,150,255])
+# draw_line(450,245,450,38,screen,[0,150,255])
+#
+# #horizontal lines
+# draw_line(50,255,250,255,screen,[0,150,255])
+# draw_line(50,245,250,245,screen,[0,150,255])
+#
+# draw_line(255,255,450,255,screen,[0,150,255])
+# draw_line(255,245,450,245,screen,[0,150,255])
 
 display(screen)
 save_extension(screen, 'img.png')
